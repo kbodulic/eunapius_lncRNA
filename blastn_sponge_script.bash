9c4f0218be06ae2adfa8a30d5d3a752f3ff828a0
kbodulic <43640978+kbodulic@users.noreply.github.com>
2020-08-17 12:59:45 +0200

    Add files via upload

--- blastn_sponge_script.bash
+++ blastn_sponge_script.bash
@@ -5,7 +5,9 @@ DATABASE=$2
 WINDOW=$3
 LEVEL=$4
 DUSTMASKER=path/to/dustmasker
-PERL_SCRIPT=/path/to/rename.pl
+PERL_SCRIPT_RENAME=/path/to/rename.pl
+MAKEBLASTDB=/path/to/makeblastdb
+BLASTN=/path/to/blastn
 
 DUSTMASKER_OUTPUT=${DATABASE%.*}_w{$WINDOW}_l{$LEVEL}.fa 
 $DUSTMASKER -in $DATABASE -infmt fasta -outfmt fasta -window $WINDOW -level $LEVEL -out $DUSTMASKER_OUTPUT 
@@ -20,15 +22,15 @@ cat $DATABASE | grep ">" > $GREP_OUTPUT
 
 PERL_OUTPUT=${TR_OUTPUT%.*}_replaced_headers.fa
 
-perl $PERL_SCRIPT $GREP_OUTPUT $TR_OUTPUT > $PERL_OUTPUT 
+perl $PERL_SCRIPT_RENAME $GREP_OUTPUT $TR_OUTPUT > $PERL_OUTPUT 
 
 
-makeblastdb -in $PERL_OUTPUT -input_type fasta -dbtype nucl -title ${TR_OUTPUT%.*}  
+$MAKEBLASTDB -in $PERL_OUTPUT -input_type fasta -dbtype nucl -title ${TR_OUTPUT%.*}  
 
 QUERY_SEQUENCE_BLAST_VAR=$(basename $QUERY_SEQUENCE)
 DATABASE_BLAST_VAR=$(basename $DATABASE)
 BLAST_OUTPUT=sponges_blast.outfmt6
 
-blastn -task blastn -query $QUERY_SEQUENCE -db $PERL_OUTPUT -num_threads 6 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend qlen sstart send slen evalue bitscore" -out $BLAST_OUTPUT 
+$BLASTN -task blastn -query $QUERY_SEQUENCE -db $PERL_OUTPUT -num_threads 10 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend qlen sstart send slen evalue bitscore" -out $BLAST_OUTPUT 
 
 
--- get_msa_seq.bash
+++ get_msa_seq.bash
@@ -5,13 +5,17 @@ QUERY=$1
 DATABASE=$2
 TRANS=$3
 
-ls | grep $TRANS | parallel cut -f1,2,3 {} ">" {.}.bed 
+PARALLEL=/path/to/parallel
+BEDTOOLS=/path/to/bedtools
+SAMTOOLS=/path/to/samtools
+
+ls | grep $TRANS | $PARALLEL cut -f1,2,3 {} ">" {.}.bed 
 
 samtools faidx $QUERY
 
 
 samtools faidx $DATABASE
 
-ls | grep \.bed | parallel bedtools getfasta -fi $DATABASE -bed {} -fo {.}_pre.fa -name
+ls | grep \.bed | $PARALLEL $BEDTOOLS getfasta -fi $DATABASE -bed {} -fo {.}_pre.fa -name
 
-ls | grep -Po ".*(?=_msa_${3}_pre\.fa)" | parallel samtools faidx $QUERY {} ">>" {}_msa_${3}_pre.fa
+ls | grep -Po ".*(?=_msa_${3}_pre\.fa)" | $PARALLEL $SAMTOOLS faidx $QUERY {} ">>" {}_msa_${3}_pre.fa
